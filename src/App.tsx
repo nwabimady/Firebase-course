effdb739bf562373899d7223916a09d811b241e2
nwabimady <nwabimady@gmail.com>
2023-08-29 21:52:39 +0100

    google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { FC } from "react";
+import { Main } from "./components/main";
+import "./App.css";
+import { UserProvider } from "./user-provider";
+export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserProvider>
+      <Main />
+    </UserProvider>
   );
-}
-
-export default App;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZhAo4ozanWNEvTbAap-P1kcf5qOJWlnw",
+  authDomain: "trusty-spanner-370709.firebaseapp.com",
+  projectId: "trusty-spanner-370709",
+  storageBucket: "trusty-spanner-370709.appspot.com",
+  messagingSenderId: "32473887623",
+  appId: "1:32473887623:web:34b5a0f32650750e51d922",
+  measurementId: "G-SRRFWYNLDQ",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
