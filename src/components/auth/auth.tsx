6307b954c4769461557614f21fa41e7e01003d51
nwabimady <nwabimady@gmail.com>
2023-08-29 22:03:58 +0100

    Mail Authentication

--- src/components/auth/auth.tsx
+++ src/components/auth/auth.tsx
@@ -1,12 +1,15 @@
 import { FC } from "react";
 import { GoogleAuth } from "./google-auth";
+import { MailRegister } from "./mail-register";
 export const Auth: FC = () => {
   return (
     <div className="contentGrid">
       <div>
         <GoogleAuth />
       </div>
-      <div>Mail Register</div>
+      <div>
+        <MailRegister />
+      </div>
       <div>Mail Login</div>
     </div>
   );
